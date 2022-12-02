95b809c97e05ff0d972e1f2a5dc5a1229651d390
CamdenGrieh <jacobdale98@aol.com>
2022-12-02 12:51:23 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import RuleInterface from "./interfaces/RuleInterfaces";
   let fizzRule: RuleInterface = new FizzRule;
   let buzzRule: RuleInterface = new BuzzRule;
   let fizzBuzzRule: RuleInterface = new FizzBuzzRule;
-  let rules: RuleInterface[] = [fizzBuzzRule, fizzRule,buzzRule];
+  // Stricter rules must run first otherwise the for loop will pass iterate to the next number
+  let rules: RuleInterface[] = [fizzBuzzRule, fizzRule,buzzRule]; 
 
   const game = new Game(rules)
 
